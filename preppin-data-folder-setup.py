3a1374f13df91f07f1526e63db22aad16ab5c451
Kelly Gilbert <gilbert.kelly.lynn@gmail.com>
2021-01-15 13:05:36 -0500

    Added year folder structure, added README builder

--- preppin-data-folder-setup.py
+++ preppin-data-folder-setup.py
@@ -18,36 +18,53 @@ Requirements:
   - main directory and current year/week variables manually updated below
 """
 
-# update the year/week to add and enter your main directory path
-year = 2021
-week = 2    # week to add
-prev_year = 2021
-prev_week = 1
-main_dir = 'C:\\projects\\preppin-data-challenge'
-
-
 
 from os import chdir, mkdir
 from shutil import copy2
 
 
+# update the year/week to add and enter your main directory path
+year = 2021
+week = 2    # week to add
+prev_year = 2021
+prev_week = 1
+main_dir = 'enter main path here'
+
 chdir(main_dir)
+yr_wk = str(year) + '-' + ('0' + str(week))[-2:]
+prev_yr_wk = str(prev_year) + '-' + ('0' + str(prev_week))[-2:]
+
 
 # create the weekly folder and inputs/outputs folders
-new_dir = '.\\preppin-data-' + str(year) + '-' + ('0' + str(week))[-2:]
-new_file = 'preppin-data-' + str(year) + '-' + ('0' + str(week))[-2:] + '.py'
+new_dir = '.\\' + str(year) + '\\preppin-data-' + yr_wk
+new_file = 'preppin-data-' + yr_wk + '.py'
 
 mkdir(new_dir)
 mkdir(new_dir + '\\inputs')
 mkdir(new_dir + '\\outputs')
 
 
-# copy the previous week's script into the main folder as a starter
-prev_dir = '.\\preppin-data-' + str(prev_year) + '-' + ('0' + str(prev_week))[-2:]
-prev_file = 'preppin-data-' + str(prev_year) + '-' + ('0' + str(prev_week))[-2:] + '.py'
+# copy the previous week's script into the main folder as a starter file
+prev_dir = '.\\' + str(year) + '\\preppin-data-' + prev_yr_wk
+prev_file = 'preppin-data-' + prev_yr_wk + '.py'
     
 copy2(prev_dir + '\\' + prev_file, new_dir + '\\' + new_file)
 
 
-# change the working directory to the new week's folder
-chdir(new_dir)
+# generate the markdown file starter
+md = '# Preppin\' Data ' + str(year) + ' Week ' + str(week) + chr(10)
+md += chr(10)
+
+md += '## Python' + chr(10)
+md += '<a href="preppin-data-' + yr_wk + '.py">' + chr(10)
+md += '<img src="img-python-code-' + yr_wk + '.png?raw=true" alt="Python code">' + chr(10)
+md += '</a>' + chr(10)
+md += chr(10)
+
+md += '## Alteryx' + chr(10)
+md += '<a href="/preppin-data-' + yr_wk + '.yxmd">' + chr(10)
+md += '<img src="img-alteryx-' + yr_wk + '.PNG?raw=true" alt="Alteryx workflow">' + chr(10)
+md += '</a>'
+
+with open(new_dir + "\\README.md", "w") as text_file:
+    text_file.write(md)
