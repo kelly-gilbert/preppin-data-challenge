0dcc09a0f8f9e9a7c59cc8ec26ca253f0643a11c
Kelly Gilbert <gilbert.kelly.lynn@gmail.com>
2021-05-06 21:04:51 -0400

    Corrected week number and link in comment at top of .py file

--- 2021/preppin-data-2021-11/preppin-data-2021-11.py
+++ 2021/preppin-data-2021-11/preppin-data-2021-11.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Preppin' Data 2021: Week 7 - Vegan Shopping List
-https://preppindata.blogspot.com/2021/02/2021-week-7-vegan-shopping-list.html
+Preppin' Data 2021: Week 7 - Cocktail Profit Margins
+https://preppindata.blogspot.com/2021/03/2021-week-11-cocktail-profit-margins.html
  
 - Input the dataset 
 - Split out the recipes into the different ingredients and their measurements
