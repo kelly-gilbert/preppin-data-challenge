56c61dc7ae32c9e33fbde1f5efa9f39746cc638e
Kelly Gilbert <48848599+kelly-gilbert@users.noreply.github.com>
2021-12-21 22:27:17 -0500

    Added files for week 2020-04

--- 2020/preppin-data-2020-04/README.md
+++ 2020/preppin-data-2020-04/README.md
@@ -1,13 +1,17 @@
-<h6><a href="..\preppin-data-2020-03\README.md">◀  Prev Week</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="..\preppin-data-2020-05\README.md">Next Week  ▶</a></h6>
+<h6><a href="..\preppin-data-2020-03\README.md">◂  Prev Week</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="..\preppin-data-2020-05\README.md">Next Week  ▶</a></h6>
 
-# Preppin' Data 2020 Week 04
+# Preppin' Data 2020 Week 4
 
-[Challenge description](https://preppindata.blogspot.com/)
+[Challenge description](https://preppindata.blogspot.com/2020/01/2020-week-4.html)
 
 What I learned/practiced this week:
-*
-*
-*
+* Practiced: replacing values using a dictionary
+* Practiced: reshaping (pivot_table)
+* Practiced: date parsing
+* Learned: pivot_table and groupby drop records with null indexes
+
+Note: both of my solutions have three extra records that were not in the provided solution:
+<img src="img-extra-rows-in-mine-2020-04.png?raw=true" alt="Screen shot of extra records in my solution">
 
 ## Python
 <a href="preppin-data-2020-04.py">
--- 2020/preppin-data-2020-04/preppin-data-2020-04.py
+++ 2020/preppin-data-2020-04/preppin-data-2020-04.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Preppin' Data 2020: Week 04 - challenge title goes here
-https://preppindata.blogspot.com/ - challeng url goes here
+Preppin' Data 2020: Week 04
+https://preppindata.blogspot.com/2020/01/2020-week-4.html
 
 - Input data
 - Change the Question Number for the question asked
-
-
 - Clean the Country and Store names
   - We only want English names for places
 - Clean up the dates and times to create a 'Completion Date' as a Date Time field
@@ -24,7 +22,7 @@ https://preppindata.blogspot.com/ - challeng url goes here
 - Output the data
 
 Author: Kelly Gilbert
-Created: 2020-MM-DD
+Created: 2021-12-20
 Requirements:
   - input datasets:
       - PD 2020 Wk 4 Input.csv
@@ -35,9 +33,15 @@ Requirements:
 
 
 import datetime as dt
-from dateutil.relativedelta import relativedelta
-from numpy import ceil, NaN, timedelta64, where
-from pandas import pivot_table, read_csv, to_datetime
+from numpy import nan, where
+from pandas import pivot_table, read_csv, to_timedelta, to_datetime
+
+
+def age(begin, end):
+    """
+    calculates the number of full years between begin and end
+    """
+    return end.year - begin.year # - ((end.month, end.day) < (begin.month, begin.day))
 
 
 def split_dict(in_dict):
@@ -80,35 +84,47 @@ df['Country'] = df['Country'].str.lower().replace(split_dict(country_dict)).str.
 df['Store'] = df['Store'].str.lower().replace(split_dict(store_dict)).str.title()
 
 
-# pivot questions into columns
-df_p = df.pivot_table(values='Answer', columns='Question Number', 
-                      index=[c for c in df.columns if c not in['Answer', 'Question Number']],
-                      aggfunc='first').reset_index()
+# summarize response attributes by Response #
+# pivot_table excludes records if any of these cols are null, such as the DoB
+df_r = df.drop_duplicates(subset='Response')[[c for c in df.columns if c not in ['Question Number', 'Answer']]]
 
+
+# pivot questions by Response # and merge back to the attributes
+df_p = df.pivot_table(index=['Response'], values='Answer', columns='Question Number', aggfunc='first')\
+         .reset_index()\
+       .merge(df_r, how='inner', on='Response')
+
+
+# rename the columns
+#     I chose to pivot using the numeric column names vs. merging the question list, because it would 
+#     save memory for a large dataset vs. repeating the question for every row.
 question_dict = {n : q for n, q in zip(df_q['Number'], df_q['Question'])}
 df_p.columns = [question_dict.get(c, c) for c in df_p.columns]
 
 
-# clean date and time
+# clean date and time, create Completion Date column
 df_p['Date'] = to_datetime(df_p['What day did you fill the survey in?'],
                            dayfirst=True, errors='coerce')
-max_yr = int(df_p['Date'].dt.year.max())
+
 df_p['Date'] = where(df_p['Date'].isna(), 
                      to_datetime(df_p['What day did you fill the survey in?']
-                               .str.replace('.* - (.*)', '\\1 ' + str(max_yr), regex=True)),
+                               .str.replace('.* - (.*)', '\\1 ' + str(CURRENT_DATE.year), regex=True)),
                      df_p['Date'])
-
-
-df_p['Time'] =to_datetime(df_p['What time did you fill the survey in?']\
-                              .str.replace('.', ':', regex=False)\
-                              .str.replace('24(\:?\d{2})', '00\\1', regex=True)\
-                              .str.replace('(\d{2})(\d{2})', '\\1:\\2', regex=True))
     
-df_p['Completion Date'] = to_datetime(df_p['Date'].astype(str) + ' ' + df_p['Time'].dt.strftime('%H:%M:%S'))
+df_p[['Hour', 'Minute', 'AMPM']] = df_p['What time did you fill the survey in?']\
+                                       .str.replace('.', ':', regex=False)\
+                                       .str.extract('(\d{1,2}):?(\d{2})\s?(.*)')
+    
+df_p['Hour'] = df_p['Hour'].astype(int) + where(df_p['AMPM'].str.lower().str.contains('pm'), 12, 0)
+df_p['Minute'] = df_p['Minute'].astype(int)
+
+df_p['Completion Date'] = df_p['Date']\
+                          + to_timedelta(df_p['Hour'], unit='h')\
+                          + to_timedelta(df_p['Minute'], unit='m')
 
 
 # understand the age of the customer based on their Date of Birth (DoB)
-df_p['Age of Customer'] = [relativedelta(CURRENT_DATE, d).years for d in df_p['DoB']]
+df_p['Age of Customer'] = df_p['DoB'].apply(lambda x: age(x, CURRENT_DATE))
 
 
 # remove any answers that are not a customer's first or latest
@@ -116,16 +132,16 @@ group_cols = ['Name', 'Store', 'Country']
 df_p['Result'] = where(df_p['Completion Date'] == df_p.groupby(group_cols)['Completion Date'].transform('min'),
                        'First',
                        where(df_p['Completion Date'] == df_p.groupby(group_cols)['Completion Date'].transform('max'),
-                             'Latest', NaN))
-df_p = df_p.loc[df_p['Result'].notna()]
+                             'Latest', 'Other'))
+df_p = df_p.loc[df_p['Result'] != 'Other']
 
 
 # classify NPS recommendation
 score_field = 'Would you recommend C&BSco to your friends and family? (Score 0-10)'
-df_p['Detractor'] = where(df_p[score_field].astype(int) <= 6, 1, NaN)
+df_p['Detractor'] = where(df_p[score_field].astype(int) <= 6, 1, nan)
 df_p['Passive'] = where((df_p[score_field].astype(int) >= 7) 
-                        & (df_p[score_field].astype(int) <= 8), 1, NaN)
-df_p['Promoter'] = where(df_p[score_field].astype(int) > 8, 1, NaN)
+                        & (df_p[score_field].astype(int) <= 8), 1, nan)
+df_p['Promoter'] = where(df_p[score_field].astype(int) > 8, 1, nan)
 
 
 #---------------------------------------------------------------------------------------------------
@@ -153,9 +169,6 @@ for i, solution_file in enumerate(solution_files):
     # read in the files
     df_solution = read_csv('.\\outputs\\' + solution_file)
     df_mine = read_csv('.\\outputs\\' + my_files[i])
-    
-    df_solution.drop(columns='Age of Customer', inplace=True)
-    df_mine.drop(columns='Age of Customer', inplace=True)
 
     # are the fields the same and in the same order?
     solution_cols = list(df_solution.columns)
@@ -198,23 +211,4 @@ for i, solution_file in enumerate(solution_files):
             print('Values match')
 
     print('\n')
-
-
-df_solution_compare.iloc[9]
-country = 'England'
-store = 'Clapham'
-Name = 'Juliana'
-date = '05/01/2020 13:03:00'
-df_solution[df_solution['Completion Date']==date].iloc[0]
-df_mine[df_mine['Completion Date']==date].iloc[0]
-
-
-df_mine[df_mine['Name']=='James Cagney'].iloc[0]
-
-x = df_p[df_p['Name']=='Gwilym']
-(CURRENT_DATE - x['DoB']) / timedelta64(1, 'Y')
-df_mine[df_mine['Completion Date']=='20/05/2019 23:00:00']
-
-df_p[df_p['Name']=='James Cagney'].iloc[0]
-
-df['Name'].unique()
+ 
