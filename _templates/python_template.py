5f0b9db4e90a0b3ec08251cf7b697b0b9364eb64
Kelly Gilbert <48848599+kelly-gilbert@users.noreply.github.com>
2022-07-09 10:14:26 -0400

    Updated Alteryx and Python templates to use new solution-check methods

--- _templates/python_template.py
+++ _templates/python_template.py
@@ -14,8 +14,6 @@ Requirements:
       - 
   - output dataset (for results check only):
       - 
-  - output_check module (for results check only)
-
 """
 
 
@@ -45,10 +43,11 @@ df.to_csv(r'.\outputs\output-YYYY-WW.csv', index=False)
 # check results
 #---------------------------------------------------------------------------------------------------
 
-solution_files = ['']
-my_files = ['output-YYYY-WW.csv']
-unique_cols = ['Month']
-col_order_matters = True
-round_dec = 8
+solution_files = [r'.\outputs\PD 2022 Wk 27 Bars Output.csv', 
+                  r'.\outputs\PD 2022 Wk 27 Liquid Output.csv']
+my_files = [r'.\outputs\output-2022-27-Bar.csv', r'.\outputs\output-2022-27-Liquid.csv']
+unique_cols = [['Store Name', 'Region', 'Quantity'], ['Store Name', 'Region', 'Quantity']]
+col_order_matters = False
+round_dec = 2
 
 output_check.output_check(solution_files, my_files, unique_cols, col_order_matters = False)
