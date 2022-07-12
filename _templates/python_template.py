08822fa503d527dbb5186f52c75dcb93b54bcef6
Kelly Gilbert <48848599+kelly-gilbert@users.noreply.github.com>
2022-07-12 15:52:57 -0400

    Updated python template (corrected output check)

--- _templates/python_template.py
+++ _templates/python_template.py
@@ -24,6 +24,9 @@ import pandas as pd
 # input the data
 #---------------------------------------------------------------------------------------------------
 
+with ExcelFile(r'.\inputs\', parse_dates=[]) as xl:
+    df = pd.read_excel(xl, sheet_name=)
+
 df = pd.read_csv(r'.\inputs\', parse_dates=[], dayfirst=True)
 
 
@@ -43,11 +46,11 @@ df.to_csv(r'.\outputs\output-YYYY-WW.csv', index=False)
 # check results
 #---------------------------------------------------------------------------------------------------
 
-solution_files = [r'.\outputs\PD 2022 Wk 27 Bars Output.csv', 
-                  r'.\outputs\PD 2022 Wk 27 Liquid Output.csv']
-my_files = [r'.\outputs\output-2022-27-Bar.csv', r'.\outputs\output-2022-27-Liquid.csv']
-unique_cols = [['Store Name', 'Region', 'Quantity'], ['Store Name', 'Region', 'Quantity']]
-col_order_matters = False
-round_dec = 2
+solution_files = [r'.\outputs\]
+my_files = [r'.\outputs\output-YYYY-WW.csv']
+unique_cols = [['col1']]
+col_order_matters = True
+round_dec = 8
 
-output_check.output_check(solution_files, my_files, unique_cols, col_order_matters = False)
+output_check(solution_files=solution_files, my_files=my_files, unique_cols=unique_cols, 
+             col_order_matters=col_order_matters, round_dec=round_dec)
