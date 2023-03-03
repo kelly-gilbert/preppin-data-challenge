4c67d19eead007e87c229a99f9ac5b442716236b
Kelly Gilbert <48848599+kelly-gilbert@users.noreply.github.com>
2023-03-03 10:32:49 -0500

    Added files for week 2022-48

--- 2022/preppin-data-2022-48/README.md
+++ 2022/preppin-data-2022-48/README.md
@@ -1,13 +1,13 @@
 <h6><a href="..\preppin-data-2022-47\README.md">◀  Prev Week</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;<a href="..\preppin-data-2022-49\README.md">Next Week  ▶</a></h6>
 
-# Preppin' Data 2022 Week 48
+# Preppin' Data 2022 Week 48 - Tiddlywinks Tidy
 
-[Challenge description](https://preppindata.blogspot.com/)
+[Challenge description](https://preppindata.blogspot.com/2022/11/2022-week-48-tiddlywinks-tidy.html)
 
 What I learned/practiced this week:
-*
-*
-*
+* Practiced: replacing partial strings with regex
+* Practiced: reshaping (melt)
+* Practiced: writing multiple sheets to an Excel file
 
 ## Python
 <i>click the image to view the code</i><br>
@@ -18,6 +18,12 @@ What I learned/practiced this week:
 
 ## Alteryx
 <i>click the image to download the workflow</i><br>
+<br>
+In the screen shot below, I used a Block Until Done tool to write the separate sheets to Excel. An alternative method (also included in the workflow linked below) is using Alteryx reporting tools + rendering to Excel.<br>
+<br>
+The drawback of the Block Until Done tool is that it will force the two branches to run in series (rather than parallel), which may take longer. On the other hand, using reporting tools adds unnecessary formatting, and can be difficult/impossible to pass through special characters (such as the line breaks in one of the event descriptions).<br>
+<br>
+This dataset is very small/runs quickly, so I chose Block Until Done for simplicity.<br>
 <br>
 <a href="preppin-data-2022-48.yxzp">
 <img src="img-alteryx-2022-48.png?raw=true" alt="Alteryx workflow">
--- 2022/preppin-data-2022-48/preppin-data-2022-48.py
+++ 2022/preppin-data-2022-48/preppin-data-2022-48.py
@@ -1,57 +1,122 @@
 # -*- coding: utf-8 -*-
 """
-Preppin' Data 2022: Week 48 - challenge title goes here
-https://preppindata.blogspot.com/ - challenge url goes here
+Preppin' Data 2022: Week 48 - Tiddlywinks Tidy
+https://preppindata.blogspot.com/2022/11/2022-week-48-tiddlywinks-tidy.html
 
-- Input data
-- ...
-- Output the data
+- Input the data
+- Extract the Event id from the Event field
+- Parse the competitor field into Competitor and Association
+- For the first Games Output:
+  - Reshape the data so we have a row per Game for each Event id and Competitor
+  - Potouts are denoted by a * in the Score field. Add a boolean field to indicate whether there has been a Potout
+  - Clean the Score field so that fractions are translated to decimals 
+    - e.g. 2½ should be 2.5
+- For the second Results Output:
+  - Remove the Games fields
+  - Clean the Points field so that fractions are translated to decimals 
+    - e.g. 2½ should be 2.5
+  - Extract the Event Start Date from the description and translate it to a date data type
+  - Ensure the field names are easy to understand
+- Output the datasets as separate sheets in the same Excel file
 
 Author: Kelly Gilbert
-Created: 2022-MM-DD
+Created: 2023-03-02
 Requirements:
   - input dataset:
-      - 
+      - International Federation of Tiddlywinks Associations World Singles Results.csv
   - output dataset (for results check only):
-      - 
+      - Tiddlywinks.xlsx
 """
 
 
 import pandas as pd
-from output_check import output_check    # custom function for checking my output vs. the solution
+
+
+#---------------------------------------------------------------------------------------------------
+# functions
+#---------------------------------------------------------------------------------------------------
+
+def clean_score(scores, replace_regex_dict):
+    """
+    replaces fraction characters and converts to float
+    """
+    
+    return ( scores.str.replace('*', '', regex=False)
+                   .replace(replace_regex_dict, regex=True)
+                   .astype(float) )
 
 
 #---------------------------------------------------------------------------------------------------
 # input the data
 #---------------------------------------------------------------------------------------------------
 
-with ExcelFile(r'.\inputs\', parse_dates=[]) as xl:
-    df = pd.read_excel(xl, sheet_name=)
-
-df = pd.read_csv(r'.\inputs\', parse_dates=[], dayfirst=True)
+df = pd.read_csv(r'.\inputs\International Federation of Tiddlywinks Associations World Singles Results.csv', 
+                 parse_dates=[], 
+                 dayfirst=True)
 
 
 #---------------------------------------------------------------------------------------------------
 # process the data
 #---------------------------------------------------------------------------------------------------
 
+# extract the event ID and competitor info
+df['Event id'] = df['event'].str.extract('.*?(\d+)$').astype(int)
+df[['Competitor Name', 'Association']] = df['Competitors'].str.extract('(.*?) \((.*)\)')
+
+
+# ---------- output #1 (game detail) ----------
+fractions = { '(.*)½.*' : r'\1.5',
+              '(.*)⅓.*' : r'\1.33',
+              '(.*)⅔.*' : r'\1.67' }
+
+# melt games into rows and filter out nulls
+df_out1 = ( df.melt(id_vars=['Event id', 'Competitor Name', 'note'], 
+                    value_vars=[c for c in df.columns if c[0]=='G'],
+                    value_name='Score',
+                    var_name='Game Order')
+              .query("Score == Score") )
+
+# flag potouts and fix fractions in score
+df_out1['Potout'] = df_out1['Score'].str.contains('*', regex=False)
+df_out1['Score'] = clean_score(df_out1['Score'], fractions)
+
+
+# ---------- output #2 (game detail) ----------
+
+renames = { 'W' : 'Wins',
+            'L' : 'Losses',
+            'T' : 'Ties',
+            'description' : 'Event Description',
+            'Pts' : 'Points',
+            'event' : 'Event' }
+
+# remove the game columns and rename columns
+df_out2 = ( df.drop(columns=[c for c in df.columns if c[0]=='G'])
+              .rename(columns=renames) )
+
+# clean points and extract the event date
+df_out2['Points'] = clean_score(df_out2['Points'], fractions)
+df_out2['Event Start Date'] = ( pd.to_datetime(df_out2['Event Description']
+                                                   .str.extract('(\d+ .*? \d{4}).*', expand=False))
+                                                   .dt.strftime('%Y-%m-%d') )
+                                                   
 
 #---------------------------------------------------------------------------------------------------
 # output the file
 #---------------------------------------------------------------------------------------------------
 
-df.to_csv(r'.\outputs\output-2022-48.csv', index=False)
+cols1 = ['Event id', 'Game Order', 'Competitor Name', 'Score', 'Potout', 'note']
 
+cols2 = ['Event id', 'Competitor Name', 'Event', 'Event Start Date', 'Event Description', 
+         'Association', 'Points', 'Wins', 'Losses', 'Ties']
+
+with pd.ExcelWriter(r'.\outputs\output-2022-48.xlsx') as w:
+    df_out1.to_excel(w, sheet_name='Games', index=False, columns=cols1)
+    df_out2.to_excel(w, sheet_name='Results', index=False, columns=cols2)
+    
 
 #---------------------------------------------------------------------------------------------------
 # check results
 #---------------------------------------------------------------------------------------------------
 
-solution_files = [r'.\outputs\]
-my_files = [r'.\outputs\output-2022-48.csv']
-unique_cols = [['col1']]
-col_order_matters = True
-round_dec = 8
-
-output_check(solution_files=solution_files, my_files=my_files, unique_cols=unique_cols, 
-             col_order_matters=col_order_matters, round_dec=round_dec)
+# checked using Alteryx this week
