f9026c0692dbd92cd194da3e516fc087d335ea8b
Kelly Gilbert <gilbert.kelly.lynn@gmail.com>
2023-07-12 21:44:16 -0400

    Added python files for 2023-13

---
+++ 2023/preppin-data-2023-14/preppin-data-2023-14.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Preppin' Data 2023: Week 14 - challenge title goes here
+https://preppindata.blogspot.com/ - challenge url goes here
+
+- Input data
+- ...
+- Output the data
+
+Author: Kelly Gilbert
+Created: 2023-MM-DD
+Requirements:
+  - input dataset:
+      - 
+  - output dataset (for results check only):
+      - 
+"""
+
+
+import pandas as pd
+from output_check import output_check    # custom function for checking my output vs. the solution
+
+
+#---------------------------------------------------------------------------------------------------
+# input the data
+#---------------------------------------------------------------------------------------------------
+
+with ExcelFile(r'.\inputs\', parse_dates=[]) as xl:
+    df = pd.read_excel(xl, sheet_name=)
+
+df = pd.read_csv(r'.\inputs\', parse_dates=[], dayfirst=True)
+
+
+#---------------------------------------------------------------------------------------------------
+# process the data
+#---------------------------------------------------------------------------------------------------
+
+
+#---------------------------------------------------------------------------------------------------
+# output the file
+#---------------------------------------------------------------------------------------------------
+
+df.to_csv(r'.\outputs\output-2023-14.csv', index=False)
+
+
+#---------------------------------------------------------------------------------------------------
+# check results
+#---------------------------------------------------------------------------------------------------
+
+solution_files = [r'.\outputs\]
+my_files = [r'.\outputs\output-2023-14.csv']
+unique_cols = [['col1']]
+col_order_matters = True
+round_dec = 8
+
+output_check(solution_files=solution_files, my_files=my_files, unique_cols=unique_cols, 
+             col_order_matters=col_order_matters, round_dec=round_dec)
